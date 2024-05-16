5ecb68b299bf3308b8b7c6ab79014261ada0251a
Mocbuilder Coding Creations <moc.build.channel@gmail.com>
2024-05-16 13:54:08 +0200

    Add project files.

---
+++ EasyConsoleHelper/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace EasyConsoleHelper
+{
+    public class ECH
+    {
+        public static void ColorPrint(string toPrint, ConsoleColor color)
+        {
+            ConsoleColor origColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(toPrint);
+            Console.ForegroundColor = origColor;
+        }
+
+        public static void ErrorPrint(string message)
+        {
+            ColorPrint($"Error: {message}", ConsoleColor.Red);
+        }
+    }
+}
